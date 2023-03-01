6453427ecd37d53dd417e86b455572ceb196b37c
Eldiyar1 <mc.oks1ne@gmail.com>
2023-03-01 23:03:25 +0600

    hw6

--- handlers/fsm_anketa.py
+++ handlers/fsm_anketa.py
@@ -2,7 +2,7 @@ from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from keyboards.client_kb import gender_markup
+from keyboards.client_kb import gender_markup, submit_markup, cancel_markup
 
 
 class FSMAdmin(StatesGroup):
@@ -11,12 +11,13 @@ class FSMAdmin(StatesGroup):
     gender = State()
     region = State()
     photo = State()
+    submit = State()
 
 
 async def fsm_start(message: types.Message):
     if message.chat.type == "private":
         await FSMAdmin.name.set()  # Обращяемся к классу FSMAdmin и вызываем у него метод состояния name
-        await message.answer("Как зовут?")
+        await message.answer("Как зовут?", reply_markup=cancel_markup)
     else:
         await message.answer('Пиши в лс')
 
@@ -30,7 +31,7 @@ async def load_name(message: types.Message, state: FSMContext):
             data['id'] = message.from_user.id
             data['username'] = f'@{message.from_user.username}'
         await FSMAdmin.next()  # переключатель состояния
-        await message.answer('Сколько тебе лет?')
+        await message.answer('Сколько тебе лет?', reply_markup=cancel_markup)
 
 
 async def load_age(message: types.Message, state: FSMContext):
@@ -53,7 +54,7 @@ async def load_gender(message: types.Message, state: FSMContext):
         async with state.proxy() as data:
             data['gender'] = message.text
     await FSMAdmin.next()
-    await message.answer('Откуда ты?')
+    await message.answer('Откуда ты?', reply_markup=cancel_markup)
 
 
 async def load_region(message: types.Message, state: FSMContext):
@@ -75,10 +76,36 @@ async def load_photo(message: types.Message, state: FSMContext):
     # условие не работает
         async with state.proxy() as data:
             data['photo'] = message.photo[0].file_id
+            await message.answer_photo(data["photo"],
+            caption=f"Имя: {data['name']}\nВозраст: {data['age']}\nПол: {data['gender']}\n"
+                    f"Регион: {data['region']}\nНик: {data['username']}")
+        await FSMAdmin.next()
+        await message.answer("Все верно?", reply_markup=submit_markup)
+
+
+async def submit(message: types.Message, state: FSMContext):
+    if message.text.lower() == "да":
         await state.finish()
+    elif message.text.lower() == "Давай по новой":
+        await FSMAdmin.name.set()
+        await message.answer("Как зовут?", reply_markup=cancel_markup)
+    else:
+        await message.answer('Не понял')
+
+
+async def cancel_reg(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state is not None:
+        await state.finish()
+        await message.answer("Отменено")
 
 
 def register_handlers_anketa(dp: Dispatcher):
+    dp.register_message_handler(cancel_reg, state="*", commands=['cancel'])
+    dp.register_message_handler(cancel_reg,
+                                Text(equals="cancel", ignore_case=True),
+                                state="*")
+
     dp.register_message_handler(fsm_start, commands=['reg'])
     dp.register_message_handler(load_name, state=FSMAdmin.name)
     dp.register_message_handler(load_age, state=FSMAdmin.age)
@@ -86,3 +113,4 @@ def register_handlers_anketa(dp: Dispatcher):
     dp.register_message_handler(load_region, state=FSMAdmin.region)
     dp.register_message_handler(load_photo, state=FSMAdmin.photo,
                                 content_types=['photo'])
+    dp.register_message_handler(submit, state=FSMAdmin.submit)
