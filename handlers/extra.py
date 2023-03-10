cc18849cfb740fe09464bd3adb093e573672321b
Мирлан Садамбеков <sadambekovmirlan9@gmail.com>
2023-03-10 14:40:09 +0600

    hghghg

--- handlers/extra.py
+++ handlers/extra.py
@@ -1,5 +1,30 @@
+import requests
 from aiogram import types, Dispatcher
+from bs4 import BeautifulSoup
+from selenium.webdriver.chrome import webdriver
+
 from config import bot
+
+async def text(massage: types.Message):
+    url = "https://ru.wikipedia.org/w/index.php?go=Перейти&search=" + massage.text
+    request = requests.get(url)
+    soup = BeautifulSoup(request.text, "html.parser")
+
+    links = soup.find_all("div", class_="mw-search-result-heading")
+
+    if len(links) > 0:
+        url = "https://ru.wikipedia.org" + links[0].find("a")["href"]
+    option = webdriver.ChromeOptions()
+    option.add_argument('headless')
+    driver = webdriver.Chrome(chrome_options=option)
+    driver.get(url)
+
+    driver.execute_script("window.scrollTo(0,200)")
+    driver.save_screenshot("img.png")
+    driver.close()
+
+    photo = open("img.png", 'rb')
+    await bot.send_photo(massage.chat.id, photo=photo, caption=f"ссылка:{url}")
 
 
 async def bad_word(message: types.Message):
@@ -24,5 +49,9 @@ async def bad_word(message: types.Message):
     else:
         await message.answer("Пиши в группе")
 
+
+
 def register_handlers_extra(dp: Dispatcher):
+    dp.register_message_handler(text)
     dp.register_message_handler(bad_word)
+
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import types
+import requests
+from aiogram.bot import bot
+from aiogram.dispatcher import storage
+from bs4 import BeautifulSoup
 from aiogram import executor  # для запуска бота
 from handlers import client, callback, admin, fsm_anketa, extra
 from config import dp
 import logging
+from selenium import webdriver
+from aiogram import types, executor, Dispatcher, Bot
+
 
 client.register_handlers_client(dp)
 callback.register_handlers_callback(dp)
@@ -10,6 +18,9 @@ fsm_anketa.register_handlers_anketa(dp)
 extra.register_handlers_extra(dp)
 
 
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     executor.start_polling(dp, skip_updates=True)
+
